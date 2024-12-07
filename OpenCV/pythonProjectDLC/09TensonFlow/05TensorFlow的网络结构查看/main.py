2b36d44e328f80618e8fdd54885e1b1ce16b52cd
wusansan <1832241264@qq.com>
2024-12-07 19:58:16 +0800

    111

--- OpenCV/pythonProjectDLC/09TensonFlow/05TensorFlow的网络结构查看/main.py
+++ OpenCV/pythonProjectDLC/09TensonFlow/05TensorFlow的网络结构查看/main.py
@@ -45,13 +45,15 @@ model.compile(optimizer=optimizer, loss="mean_squared_error")
 # 2) plot_model函数保存网络结构图
 # 本地虚拟仿真平台命令行中python -m pip install pydot==1.4.1
 # install graphviz (see instructions at https://graphviz.gitlab.io/download/) for plot_model/model_to_dot to work
-tf.keras.utils.plot_model(model, to_file="model.png", show_shapes=True)
+# tf.keras.utils.plot_model(model, to_file="model.png", show_shapes=True)
 
-# 3)netron
+# 3)netron 虚拟仿真平台训练打开h5文件
 
 # 4)tensorboard方法
-# epoches = 500
-# # 在计算图中添加模型
-# tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="./logs")
-# model.fit(x_train, y_train, epochs=epoches, callbacks=[tensorboard_callback])
-
+epoches = 500
+# 在计算图中添加模型
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="./logs")
+# 把训练过程数据添加到模型中，便于显示
+model.fit(x_train, y_train, epochs=epoches, callbacks=[tensorboard_callback])
+# cd D:\projects\OpenCV\pythonProjectDLC\09TensonFlow\05TensorFlow的网络结构查看
+# "D:\soft\FS\FS_AISIMS\tools\Python\python.exe" "D:\soft\FS\FS_AISIMS\tools\Python\Scripts\tensorboard.exe"  --logdir "logs"
